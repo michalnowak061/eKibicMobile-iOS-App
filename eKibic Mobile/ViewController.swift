bd8fd894cad43dbe05b550826c0aa314ed567ebb
Michał Nowak <michal.nowak061@gmail.com>
2020-09-11 18:10:39 +0200

    Initial Commit

---
+++ eKibic Mobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  eKibic Mobile
+//
+//  Created by Michał Nowak on 11/09/2020.
+//  Copyright © 2020 mnowak061. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
